5ab30127b5a55f7d2e9dd34c65ab8035f04376cf
黄振桐 <huangzhentong@keytop.com.cn>
2019-12-20 17:57:31 +0800

    提交0.1.5更新

--- Example/KTMapSDK/KTViewController.m
+++ Example/KTMapSDK/KTViewController.m
@@ -29,27 +29,11 @@
     [KTSDK presentSDK:self mapAPIKey:@"745dc31cded658ccfedfdc33684a75eb"];
     return;
     
-//    Class aMapClass = NSClassFromString(@"KTDMapManager");
-//    if (aMapClass == nil) {
-//        NSLog(@"未集成DMap");
-//        return;
-//    }
-//    SEL defaultServiceSel = NSSelectorFromString(@"shareInstance");
-//    if ([aMapClass respondsToSelector:defaultServiceSel]) {
-//        id (*sharedServices)(id,SEL) = (id (*) (id,SEL))objc_msgSend;
-//        id aMap =  sharedServices(aMapClass,defaultServiceSel);
-//
-//
-//        void(*initWithDMapWithCode)(id,SEL,NSString *,NSString *) = (id(*)(id,SEL,NSString *code,NSString *url))objc_msgSend;
-//        initWithDMapWithCode(aMap,NSSelectorFromString(@"initWithDMapWithCode:url:"),@"hyjg",@"https://test.seeklane.com/test/hyjg/index.html");
-//    }
     
     KTDMapManager *manager = [KTDMapManager shareInstance];
     manager.code = @"hyjg";
     manager.url = @"https://test.seeklane.com/test/hyjg/index.html";
     [[NSNotificationCenter defaultCenter] postNotificationName:PushToDMapViewController object:@{@"viewController":self,@"long":@(1),@"lat":@(2)}];
-//    KTDMapViewController *viewController =[[KTDMapViewController alloc] initWithCode:@"hyjg" withURL:@"https://test.seeklane.com/test/hyjg/index.html"];
-//    [self presentViewController:viewController animated:true completion:nil];
 
     
 }
--- README.md
+++ README.md
@@ -32,17 +32,34 @@ pod 'KTMapSDK'
 
 KTMapSDK is available under the MIT license. See the LICENSE file for more info.
 
+科拓速停车室内定位SDK
 
 //集成
 pod 'KTMapSDK'
 
+//-------
+cocoapods 找不到库
+
+方法1
+pod repo update
+
+方法2
+前往~/Library/Caches/CocoaPods/ 
+删除search_index.json 
+再重新 pod search KTMapSDK
+
 
 //需要在plist 文件内添加权限
-App Transport Security Settings 
 
+App Transport Security Settings 
 Privacy - Bluetooth Always Usage Description
 Privacy - Location Always and When In Use Usage Description
 Privacy - Location When In Use Usage Description
 
+
+//使用方式
 // 传入一个viewController 以及高德APIKey
 [KTSDK presentSDK:self mapAPIKey:@""];
+
+
+
